3479918d82522528f219e18777b6e2129e71965d
Kristina Motte <krmot@telenor.dk>
2022-09-24 19:50:54 +0100

    finish with search

--- HealthyPet/HealthyPet/Features/SelectOption/SelectOptionViewController.swift
+++ HealthyPet/HealthyPet/Features/SelectOption/SelectOptionViewController.swift
@@ -12,22 +12,35 @@ protocol SelectOptionViewControllerDelegate: AnyObject {
 }
 
 class SelectOptionViewController: UIViewController {
+    // MARK: - Outlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    // MARK: - View Model
     var viewModel: SelectOptionViewModel?
     
+    // MARK: - Delegate
     weak var delegate: SelectOptionViewControllerDelegate?
+    
+    // MARK: - Helpers
+    private var filteredBreeds: [String] = []
+    
+    private var isSearchBarEmpty: Bool {
+        searchBar.text?.isEmpty ?? true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        searchBar.delegate = self
         configureUI()
         configureTableView()
+        self.hideKeyboardWhenTappedAround()
     }
     
+    // MARK: - Private methods
     private func configureUI() {
         titleLabel.font = Theme.Fonts.openSansLight24
         titleLabel.textColor = Theme.Colors.black
@@ -42,20 +55,33 @@ class SelectOptionViewController: UIViewController {
         tableView.separatorStyle = .none
         SelectBreedTableViewCell.register(in: tableView)
     }
-
-    @IBAction func didTapDoneButton(_ sender: UIButton) {
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        guard let viewModel = viewModel else { return }
+        
+        filteredBreeds = viewModel.breeds.filter { $0.lowercased().contains(searchText.lowercased()) }
+        
+        tableView.reloadData()
+    }
+    
+    @IBAction private func didTapDoneButton(_ sender: UIButton) {
         dismiss(animated: true) {
             self.delegate?.didChoose(option: self.viewModel?.selectedOption ?? "")
         }
     }
 }
 
+// MARK: - UITableViewDelegate & UITableViewDataSource
 extension SelectOptionViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !isSearchBarEmpty {
+            return filteredBreeds.count
+        }
+        
         let breeds = viewModel?.breeds ?? []
         return breeds.count
     }
@@ -63,7 +89,7 @@ extension SelectOptionViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let viewModel = viewModel else { return UITableViewCell() }
         
-        let option = viewModel.breeds[indexPath.row]
+        let option = isSearchBarEmpty ? viewModel.breeds[indexPath.row] : filteredBreeds[indexPath.row]
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: SelectBreedTableViewCell.identifier) as? SelectBreedTableViewCell {
             cell.configure(with: option)
@@ -77,19 +103,23 @@ extension SelectOptionViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let viewModel = viewModel else { return }
         
-        let option = viewModel.breeds[indexPath.row]
+        let option = isSearchBarEmpty ? viewModel.breeds[indexPath.row] : filteredBreeds[indexPath.row]
         viewModel.selectedOption = option
     }
 }
 
+// MARK: - UISearchBarDelegate
 extension SelectOptionViewController: UISearchBarDelegate {
-    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
-        guard let viewModel = viewModel else { return false }
-        
-        return true
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchBar.text ?? "")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterContentForSearchText(searchBar.text ?? "")
     }
 }
 
+// MARK: - StoryboardController
 extension SelectOptionViewController: StoryboardController {
     static var storyboard: Storyboard {
         .AddNewPet
